1971488ba322aa94aa9479c3d926188480327bd6
Aliaksandr Karabeinikau <xansandr@yandex.by>
2024-07-07 01:39:56 +0300

    changed filename

---
+++ HW_1-2/HW_3.js
@@ -0,0 +1,15 @@
+//  HW3
+console.log("HW3");
+//////////////////////
+
+/////// TASK 1 ///////
+
+//////////////////////
+
+{
+    let array = [];
+    for (i=1;i>=50;i++){
+        array.push(i)
+    }
+    console.log(array)
+}
